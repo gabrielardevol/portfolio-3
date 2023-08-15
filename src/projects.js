b7cb3fc4e2bd776a07a007765f6d5de55a82b513
Gabriel Ardevol <artsdevol@gmail.com>
2023-08-15 12:15:11 +0200

    working on projects json

--- src/projects.js
+++ src/projects.js
@@ -1,6 +1,7 @@
 const projects = {
   "orbits": {
     title: "orbits",
+    structure: "",
     imgPath: "images/orbits",
     date: "8/2023",
     hashtags: ["react", "front-end"],
@@ -10,14 +11,37 @@ const projects = {
   },
   "pigs": {
     title: "pigs",
+    structure: "",
     imgPath: "images/pigs",
     date: "12/2022",
     hashtags: ["ruby"],
     links: {
       "gitHub": "a"
     }
-
   },
+  "esclat": {
+    title: "esclat",
+    structure: "<p>{t('projects.esclat.content')}</p>",
+    imgPath: "images/esclat",
+    date: "5/2023",
+    hashtags: ["UI", "javascript"],
+    links: {
+      "web": "https://associacioesclat.com/edc.html",
+      "github": "https://github.com/gabrielardevol/esclat"
+    }
+  },
+  "celler": {
+    title: "celler",
+    structure: "<p>{t('projects.celler.content')}</p>",
+    imgPath: "images/celler",
+    date: "1/2023",
+    hashtags: ["ruby", "rails"],
+    links: {
+      "web": "https://cellerardevol.herokuapp.com/",
+      "github": "https://github.com/gabrielardevol/celler-ardevol"
+    }
+
+  }
 }
 
 export default projects
