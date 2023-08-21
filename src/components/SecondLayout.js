65958be2b654976788c7b302a36175576cc779bf
Gabriel Ardevol <artsdevol@gmail.com>
2023-08-21 02:52:00 +0200

    project images update

--- src/components/SecondLayout.js
+++ src/components/SecondLayout.js
@@ -1,16 +1,13 @@
 import React, { useRef, useState, useEffect } from 'react';
 
 import { useTranslation } from 'react-i18next';
-
 import i18n from '../i18n.js'; // no esborrar
-
 import projects from '../projects';
 
 const SecondLayout = React.forwardRef((props, ref) => {
   const { t } = useTranslation();
   const project = props.project
   const imgRef = useRef()
-  // const images = projects[project].images
   const images = projects[project].images
 
   let [imageIndex, setImageIndex] = useState(0)
@@ -22,20 +19,9 @@ const SecondLayout = React.forwardRef((props, ref) => {
     const newIndex = imageIndex - 1;
     setImageIndex(newIndex);
   }
-
-  // const resetImgIndex = (e) => {
-  //   if (e.target.classList.contains("prj")) {
-  //     setImageIndex(0)
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   window.addEventListener('click', (e) => resetImgIndex(e));
-  //   return () => {
-  //     window.removeEventListener('click', (e) => resetImgIndex(e));
-  //   };
-  // }, []);
-
+  useEffect(() => {
+    setImageIndex(0);
+  }, [project]);
 
   return (
     <div ref={ref} id="second-layout" style={{gridArea: "second-layout"}} >
@@ -44,7 +30,7 @@ const SecondLayout = React.forwardRef((props, ref) => {
         {imageIndex != 0 && <button className="skip-skelleton" id="previmg-button" onClick={prevImg}> &lt; </button>}
         {imageIndex != images.length-1 && <button className="skip-skelleton" id="nextimg-button" onClick={nextImg}>&gt; </button>}
         <div style={{height: "100%", width: "100%", overflow: "auto"}}>
-          <div style={{display: "flex", justifyContent: "center"}}>
+          <div style={{display: "flex", justifyContent: "center", height: "100%"}}>
             <img id="second-layout-image" ref={imgRef} data-index={imageIndex} src={images[imageIndex]} />
           </div>
         </div>
